f0dc94f4dcbf73ed3e71fd1ace892612c5435296
Manuel Hanisch <manuelhanisch@gmail.com>
2024-11-12 18:33:31 +0100

    OERC-51: Adjusting texts, translations and headers

--- src/pretalx/cfp/flow.py
+++ src/pretalx/cfp/flow.py
@@ -448,7 +448,7 @@ class QuestionsStep(GenericFlowStep, FormFlowStep):
     def oercamp_check_and_add_angel_tag(submission, event):
 
         ANGEL_TAG_TAG = "Boten-Engel"
-        OERCAMP_QUESTION_ANGEL = "Die Session soll vom Boten-Engel vorgestellt werden"
+        OERCAMP_QUESTION_ANGEL = "Die Session soll vom Boten-Engel vorgestellt werden."
 
         submission_answers = submission.answers.all().select_related("question")
 
