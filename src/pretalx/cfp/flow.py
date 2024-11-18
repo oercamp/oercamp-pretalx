4fd437800cc69eea0e667daef422f1f0ddbe5f30
Manuel Hanisch <manuelhanisch@gmail.com>
2024-11-18 15:24:56 +0100

    OERC-51: Fixing display of wrong questions for submissions with access_code

--- src/pretalx/cfp/flow.py
+++ src/pretalx/cfp/flow.py
@@ -427,6 +427,18 @@ class QuestionsStep(GenericFlowStep, FormFlowStep):
         result["target"] = ""
         result["track"] = info_data.get("track")
         result["submission_type"] = info_data.get("submission_type")
+
+        # NOVA FIX: adding access_code check, because for forms initiated with access_code
+        # the submission_type is not set, and then all questions are shown, including the ones
+        # that are limited to another submission_type.
+        access_code = getattr(self.request, "access_code", None)
+        if(
+            result["submission_type"] is None and
+            access_code is not None and
+            isinstance(access_code.submission_type_id, int)
+        ):
+            result["submission_type"] = access_code.submission_type_id
+
         if not self.request.user.is_anonymous:
             result["speaker"] = self.request.user
         return result
