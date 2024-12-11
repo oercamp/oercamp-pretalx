2150cb9ee6ac462244bedcba25bcdfd6a42c3a4d
Manuel Hanisch <manuelhanisch@gmail.com>
2024-12-11 16:58:06 +0100

    OERC-31: adding js initial code to initialize widget on buttons

---
+++ src/pretalx/static/common/js/pretix_widget.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    window.pretixWidgetCallback = function () {
+        window.PretixWidget.addCloseListener(function () {
+            console.log("Widget has been closed!");
+            location.reload();
+        });
+    }
+    function pretixWidgetOpen(target_url) {
+        window.PretixWidget.open(
+            target_url
+        )
+    }
+
+
+    // Select all elements with the class `btn-pretix-purchase-ticket`
+    const buttons = document.querySelectorAll(".btn-pretix-purchase-ticket");
+
+    // Loop through each button and attach the click event
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent the default link behavior
+            const targetUrl = button.getAttribute("href"); // Get the URL from the href attribute
+            pretixWidgetOpen(targetUrl); // Call your function with the target URL
+        });
+    });
+})
